7eba95a8f8fb584d83d69f5f2b46e30118180680
Bruno Dias <brunosousa1997@hotmail.com>
2022-09-12 21:59:33 -0300

    vai se fude xcode kkkkkkkkkkkk

---
+++ swift-part2-/eggplant-browniee/Controllers/RefeicoesTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  RefeicoesTableViewController.swift
+//  eggplant-brownie
+//
+//  Created by Bruno Sousa on 12/09/22.
+//  Copyright © 2022 Alura. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RefeicoesTableViewController: UITableViewController {
+    
+    var refeicoes = [
+        Refeicao(nome: "Churros", felicidade: 5),
+        Refeicao(nome: "Batata Doce", felicidade: 5),
+        Refeicao(nome: "Pao na chapa", felicidade: 5)
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let refeicao = refeicoes[indexPath.row]
+        
+        cell.textLabel?.text = refeicao.nome
+        
+        return cell
+    }
+    
+    func add(_ refeicao: Refeicao){
+        refeicoes.append(refeicao)
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Passar referencia do controller para outro
+        if let viewController = segue.destination as? ViewController {
+            viewController.tableViewController = self
+        }
+    }
+}
